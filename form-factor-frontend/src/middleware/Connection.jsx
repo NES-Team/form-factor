38724bc06cbd4e5ea3e4d489120eba2db3d246f3
philipdoucla <philipminhdo@ucla.edu>
2023-11-17 21:14:11 -0800

    feat: got working websocket

--- form-factor-frontend/src/middleware/Connection.jsx
+++ form-factor-frontend/src/middleware/Connection.jsx
@@ -1,12 +1,49 @@
+import { state } from '@abandonware/noble';
 import { useEffect, useState } from 'react';
+
+const [AX, AY, AZ, GX, GY, GZ] = [0,1,2,3,4,5,6,7]
+
+
+
 
 export default function Connection() {
   const [sensorData, setSensorData] = useState({ descriptor: '', sensorValue: NaN });
   const [socket, setSocket] = useState(null);
+  const [stateArr, setStateArr] = useState([0,0,0,0,0,0])
+
+  function descriptorParser() {
+
+    let i = -1
+    switch (sensorData.descriptor) {
+      case "ax":
+        i = AX
+        break
+      case "ay":
+        i = AY
+       break
+      case "az":
+        i = AZ
+        break
+      case "gx":
+        i = GX
+       break
+      case "gy":
+        i = GY
+        break
+      case "gz":
+        i = GZ
+        break
+      default:
+        console.log("Error: state desc not valid")
+    }
+  
+    stateArr[i] = sensorData.sensorValue
+  }
+  
 
   useEffect(() => {
     // Establish WebSocket connection
-    const ws = new WebSocket('ws://localhost:3000'); // Replace with your server URL
+    const ws = new WebSocket('ws://localhost:8000'); // Replace with your server URL
 
     ws.onopen = () => {
       console.log('WebSocket connected');
@@ -16,6 +53,8 @@ export default function Connection() {
     ws.onmessage = (event) => {
       const receivedData = JSON.parse(event.data);
       setSensorData(receivedData); // Update state with received sensor data
+      descriptorParser(stateArr, receivedData.descriptor, receivedData.sensorValue)
+      console.log(receivedData)
     };
 
     ws.onerror = (error) => {
@@ -32,8 +71,15 @@ export default function Connection() {
   return (
     <div className="App">
       <h1>IMU Sensor Data</h1>
-      <p>Descriptor: {sensorData.descriptor}</p>
-      <p>Sensor Value: {sensorData.sensorValue}</p>
+      {/* <p>Descriptors: {sensorData.descriptor}</p>
+      <p>Sensor Value: {sensorData.sensorValue}</p> */}
+      <p>ax: {stateArr[AX]}</p>
+      <p>ay: {stateArr[AY]}</p>
+      <p>az: {stateArr[AZ]}</p>
+      <p>gx: {stateArr[GX]}</p>
+      <p>gy: {stateArr[GY]}</p>
+      <p>gz: {stateArr[GZ]}</p>
+
     </div>
   );
 }
