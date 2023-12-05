c45969167597f77eb7e0a4bf340d0e5a8ed850e1
Vinh Nguyen <vinhsnguyen86@gmail.com>
2023-12-04 19:52:20 -0800

    Expanding the badForm utility to start calculating the rest of the workouts

--- form-factor-frontend/src/utils/vinh.js
+++ form-factor-frontend/src/utils/vinh.js
@@ -13,6 +13,33 @@ export default function badForm({ax, ay, az, gx, gy, gz, exercise}){
                 return false
             }
             break
+
+        case "lateral":
+            if(
+                gx < -60 || gx > 60 ||
+                gy < -100 || gy > 100 ||
+                gz < -50 || gz > 50 || 
+                ax > 0.6 || // could be cut
+                ay < -0.4 || ay > 0.1 || 
+                az > 0.1
+                ){
+                return false
+            }
+            break
+
+        case "row":
+            if(
+                gx < -15 || gx > 15 ||
+                gy < -40 || gy > 40 ||
+                gz < -50 || gz > 50 || 
+                ax > -0.9 || // could be cut
+                ay < -0.4 || ay > 0.1 || 
+                az < -0.2 || az > 0.6
+                ){
+                return false
+            }
+            break
+
         case "jack":
             if(gx < -300 || gx > 300 || // if going too fast
                 gy < -200 || gy > 200||
@@ -23,18 +50,34 @@ export default function badForm({ax, ay, az, gx, gy, gz, exercise}){
                 return false
             }
             break
-        case "typing":
-            // lateral raise info actually
-            if(gx < -100 || gx > 100 || // if going too fast
-                gy < -300 || gy > 300||
-                gz < -100 || gz > 100 ||
-                az < 0 || // if bad form
-                ay < -0.5 || ay > 0.5 ||
-                ax < 0
+
+        case "ab":
+            if(
+                gx < -60 || gx > 60 ||
+                gy < -100 || gy > 100 ||
+                gz < -50 || gz > 50 || 
+                ax < -0.7 || ax > 0.1 ||
+                ay < 0.7 || 
+                az < -0.3 || az > 0.6
                 ){
                 return false
             }
             break
+
+        case "push":
+            if(
+                gx < -20 || gx > 20 ||
+                gy < -30 || gy > 30 ||
+                gz < -10 || gz > 10 || 
+                ax < -0.2 || ax > 0.3 ||
+                ay < -0.4 || ay > 0.1 || 
+                az > -0.8
+                ){
+                return false
+            }
+            break
+
+        
     }
     return true
 }
