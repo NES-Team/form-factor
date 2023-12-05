7bd755081006afea3c86d4ce24cd5e3d69ec835d
Vinh Nguyen <vinhsnguyen86@gmail.com>
2023-12-05 00:19:30 -0800

    calibrating the workout assesment parameters

--- form-factor-frontend/src/utils/vinh.js
+++ form-factor-frontend/src/utils/vinh.js
@@ -25,8 +25,9 @@ export default function badForm({ax, ay, az, gx, gy, gz, exercise, sliderVal}){
 
         case "lateral":
             if(ax > 0.6 || // could be cut
-                ay < -0.4 || ay > 0.1 ||
-                az > 0.1){
+                ay < -0.5 || ay > 0.2 ||
+                az < -0.1
+                ){
                 return "bad form"
             }
 
@@ -39,23 +40,25 @@ export default function badForm({ax, ay, az, gx, gy, gz, exercise, sliderVal}){
             break
 
         case "row":
-            if(ax > -0.9 || // could be cut
-                ay < -0.4 || ay > 0.1 || 
-                az < -0.2 || az > 0.6){
-                return "bad form"
-            }
-
             if(doSlowDown && (gx < -15 || gx > 15 ||
                 gy < -40 || gy > 40 ||
                 gz < -50 || gz > 50)){
                 return "slowdown"
             }
 
+            if(ax > -0.6 || // could be cut
+                ay < -0.6 || ay > 0.3 || 
+                az < -0.2 || az > 0.7){
+                return "bad form"
+                
+            }
+
             break
 
         case "jack":
-            if(az < -1 || az > 1 || // if bad form
-                ay < -0.2){
+            if(az < -1 || az > 1  // if bad form
+                // ay < -0.2
+                ){
                 return "bad form"
             }
             
@@ -67,23 +70,22 @@ export default function badForm({ax, ay, az, gx, gy, gz, exercise, sliderVal}){
             break
 
         case "crunch":
-            if(ax < -0.7 || ax > 0.1 ||
-                ay < 0.7 || 
-                az < -0.3 || az > 0.6){
+            if(ax < -0.7 || ax > 0.3 ||
+                az > 0.6){
                 return "bad form"
             }
             
-            if(doSlowDown && (gx < -60 || gx > 60 ||
-                gy < -100 || gy > 100 ||
-                gz < -50 || gz > 50)){
+            if(doSlowDown && (gx < -90 || gx > 90 ||
+                gy < -130 || gy > 130 ||
+                gz < -80 || gz > 80)){
                 return "slowdown"
             }
             break
 
         case "push":
-            if(ax < -0.2 || ax > 0.3 ||
-                ay < -0.4 || ay > 0.1 || 
-                az > -0.8){
+            if(ax < -0.4 || ax > 0.3 ||
+                ay < -0.5 || ay > 0 || 
+                az < 0.8){
                 return "bad form"
             }
             
